e5630ba4f417ace739586bca034a6262137b4fbd
Mustafa Urgupluoglu <mustafa.urgupluoglu@sigmatelecom.com>
2018-06-18 16:52:59 +0300

    arch navigation added

--- app/src/main/java/com/murgupluoglu/kotlinmvvm/MainActivity.kt
+++ app/src/main/java/com/murgupluoglu/kotlinmvvm/MainActivity.kt
@@ -2,30 +2,21 @@ package com.murgupluoglu.kotlinmvvm
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.murgupluoglu.kotlinmvvm.activity.BaseActivity
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         AndroidInjection.inject(this)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val navController = Navigation.findNavController(this@MainActivity, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(navigation, navController)
     }
 
 }

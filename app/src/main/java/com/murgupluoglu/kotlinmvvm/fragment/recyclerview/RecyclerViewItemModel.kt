cc1029eaaea39b0433f8200028a7ad0524c9d75c
Mustafa Urgupluoglu <mustafa.urgupluoglu@markakod.com>
2019-02-11 15:40:55 +0300

    ViewPager2 sample added

--- app/src/main/java/com/murgupluoglu/kotlinmvvm/fragment/recyclerview/RecyclerViewItemModel.kt
+++ app/src/main/java/com/murgupluoglu/kotlinmvvm/fragment/recyclerview/RecyclerViewItemModel.kt
@@ -1,26 +1,8 @@
 package com.murgupluoglu.kotlinmvvm.fragment.recyclerview
 
-import android.widget.ImageView
-import androidx.databinding.BindingAdapter
-import androidx.viewpager.widget.ViewPager
-import com.bumptech.glide.Glide
-
 enum class RecyclerViewItemTypes{
     ITEM_ONE,
     ITEM_VIEWPAGER
 }
 data class RecyclerViewItemModel(val viewType: Int, val id: String, val title: String, val subtitle: String, val body: String, val array: ArrayList<ViewPagerItem>?)
 data class ViewPagerItem(val title: String, val imageURL: String)
-
-@BindingAdapter("bindViewPager")
-fun bindViewPager(viewPager: ViewPager, array: ArrayList<ViewPagerItem>?) {
-    if(array != null){
-        val viewpagerAdapter = ViewPagerAdapter(array)
-        viewPager.adapter = viewpagerAdapter
-    }
-}
-
-@BindingAdapter("loadGlideUrl")
-fun loadGlideUrl(imageView: ImageView, url : String) {
-    Glide.with(imageView).load(url).into(imageView)
-}
--- app/src/main/java/com/murgupluoglu/kotlinmvvm/utils/BindingUtils.kt
+++ app/src/main/java/com/murgupluoglu/kotlinmvvm/utils/BindingUtils.kt
@@ -1,18 +1,37 @@
 package com.murgupluoglu.kotlinmvvm.utils
 
-import android.text.TextUtils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import com.murgupluoglu.kotlinmvvm.fragment.recyclerview.ViewPager2Adapter
+import com.murgupluoglu.kotlinmvvm.fragment.recyclerview.ViewPagerAdapter
+import com.murgupluoglu.kotlinmvvm.fragment.recyclerview.ViewPagerItem
 
 /**
  * Created by Mustafa Urgupluoglu on 18.01.2019.
  */
 
-@BindingAdapter("loadImage")
-fun loadImage(view: ImageView, url: String?) {
-    if (!TextUtils.isEmpty(url)) {
-        Glide.with(view).load(url).apply(RequestOptions.fitCenterTransform()).into(view)
+@BindingAdapter("bindViewPager")
+fun bindViewPager(viewPager: ViewPager, array: ArrayList<ViewPagerItem>?) {
+    if(!array.isNullOrEmpty()){
+        val viewpagerAdapter = ViewPagerAdapter(array)
+        viewPager.adapter = viewpagerAdapter
+    }
+}
+
+@BindingAdapter("bindViewPager2")
+fun bindViewPager2(viewPager: ViewPager2, array: ArrayList<ViewPagerItem>?) {
+    if(!array.isNullOrEmpty()){
+        val viewpagerAdapter = ViewPager2Adapter(array)
+        viewPager.adapter = viewpagerAdapter
+    }
+}
+
+@BindingAdapter("loadGlideUrl")
+fun loadGlideUrl(imageView: ImageView, url : String?) {
+    if(!url.isNullOrEmpty()){
+        Glide.with(imageView).load(url).into(imageView)
     }
 }
